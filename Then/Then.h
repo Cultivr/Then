f3da2ba8a35a3dde9252ec297e1aec718c3f2159
Jordan Kay <jordanekay@mac.com>
2017-11-16 19:15:54 -0800

    Initial Commit

---
+++ Then/Then.h
@@ -0,0 +1,19 @@
+//
+//  Then.h
+//  Then
+//
+//  Created by Jordan Kay on 11/16/17.
+//  Copyright © 2017 Jordan Kay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Then.
+FOUNDATION_EXPORT double ThenVersionNumber;
+
+//! Project version string for Then.
+FOUNDATION_EXPORT const unsigned char ThenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Then/PublicHeader.h>
+
+
